09abef9707940504c09a4d25a96c91b3e80ea947
tadeoabbruzzese <tadeoabbruzzese@gmail.com>
2023-11-03 17:03:27 -0300

    hago cambios en index, server y main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,10 @@
 console.log(document.querySelector('title').textContent)
 
+// Creando un cliente
+
 const socket = io.connect()
 
 socket.on('nombre', data => {
-    console.log(data)
-})
-
-socket.on('array-objetos', data => {
     console.log(data)
 })
 
@@ -14,43 +12,46 @@ socket.on('pescar', data => {
     console.log(data)
 })
 
+socket.on('array-objetos', data => {
+    console.log(data)
+})
 
-// ------------------------------------------
+// -----------------------------
 
 socket.on('mensajes', data => {
     console.log(data)
     render(data)
 })
 
-// ------------------------------------------
-// Gestion de carga de texto de los chat
-
-// ----------------------------------------------
+// -----------------------------
 
 // Renderizado de los mensajes recibidos
 
 function render(data) {
     let html = data.map(msj => {
-        return(`
+        return ( `
             <div>
-                <strong>${msj.usuario}</strong>
+                <strong>${msj.usuario}</strong>:
                 <em>${msj.mensaje}</em>
             </div>
-
         `)
     }).join(' ')
     document.querySelector('.mensajes').innerHTML = html
 }
-// -----------------------------------------------
+
+
+// -----------------------------
+// Gestion de carga de textos de los chats
 
 function agregarMensaje(e) {
     e.preventDefault()
-    const usuario = document.querySelector('#lbl-usuario').value
-    const mensaje = document.querySelector('#lbl-mensaje').value
+
+    const usuario = document.querySelector('#lbl-usuario')
+    const mensaje = document.querySelector('#lbl-mensaje')
 
     const obj = {
-        usuario: usuario.value,
-        mensaje: mensaje.value
+        usuario: usuario.value, // 'Maxi'
+        mensaje: mensaje.value // 'Hola que tal...'
     }
 
     socket.emit('nuevo-comentario', obj)
@@ -60,6 +61,4 @@ function agregarMensaje(e) {
 }
 
 const btn = document.querySelector('#btn')
-
 btn.addEventListener('click', agregarMensaje)
-
--- server.js
+++ server.js
@@ -1,44 +1,42 @@
-//IMPORTS
 import express from 'express'
 import 'dotenv/config'
 import path from 'node:path'
-import { Server } from 'socket.io'
-import { createServer } from 'node:http'
+import { Server } from "socket.io";
+import { createServer } from 'node:http';
 
-//CONFIGS
+// ! Configuración
 const PORT = process.env.PORT
 const app = express()
-const server = createServer(app)
-const io = new Server(server)
-// Middlewares
+const server = createServer(app);
+const io = new Server(server);
+
+// ! Middleware
 app.use(express.static(path.join('public')))
 
-
-let usuarioConectado = 0;
+let usuarioConectado = 0
 
 const mensajes = [
-    {usuario: 'Tadeo', mensaje: 'Hola que tal!'},
-    {usuario: 'Valentina', mensaje: 'Muy bien y vos?'},
-    {usuario: 'Rodrigo', mensaje: 'Genial!'}
+    { usuario: 'Tadeo', mensaje: 'Hola! Qué tal!'},
+    { usuario: 'Paula', mensaje: 'Muy bien! y vos?'},
+    { usuario: 'Rodrigo', mensaje: 'Genial!'}
 ]
+
 // Socket.io
 // Escucho cuando un cliente se conecte
 io.on('connection', (socket) => {
-    // console.log(socket)
+    //console.log(socket)
     console.log('Un cliente se ha conectado: ', socket.id)
     usuarioConectado++
     console.log(usuarioConectado)
-
-
     socket.on('disconnect', () => {
         console.log('Cliente desconectado', socket.id)
         usuarioConectado--
         console.log(usuarioConectado)
     })
 
-    // Emitir un mensaje
-    socket.emit('nombre', 'Tadeo')
-    socket.emit('pescar', 'Viernes')
+    // Emitir un mensaje desde el server
+    socket.emit('nombre', 'Maximiliano')
+    socket.emit('pescar', 'viernes')
     socket.emit('array-objetos', [{id: 1}, {id: 2}, {id: 3}, {id: 4}])
 
     // Recibiendo la información del formulario
@@ -48,11 +46,12 @@ io.on('connection', (socket) => {
         io.sockets.emit('mensajes', mensajes)
     })
 
-    // Emito los mensajes para llenar el chat 
+    // Emito los mensajes para llenar el chat
     socket.emit('mensajes', mensajes)
+    
+
 })
 
-//PUERTO ESCUCHANDO
 server.listen(PORT, () => {
-console.log(`Servidor corriendo en el puerto: ${PORT}`)
+    console.log(`Servidor corriendo http://localhost:${PORT}`)
 })
